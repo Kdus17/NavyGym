ba19f15825693e5becc0ec5ae51236e175ef5804
Kdus17 <kidus2460@gmail.com>
2025-08-15 11:52:31 +0300

    facilites ui imporvment and refactoring

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { Dot } from 'lucide-react';
+import React, {  useState } from 'react'
 
 
 const Facilities = () => {
@@ -54,11 +55,10 @@ const Facilities = () => {
   ];
 
   const[nav,setNav] =useState({state1 : true, state2: false, state3: false})
-  const [products,setProd] =useState([]);
-
+  const [products,setProd] = useState(equipment);
 
   return (
-    <div id='Facilities' className='flex flex-col items-center gap-4 py-6 bg-neutral-950 w-full'>
+    <div id='Facilities' className='flex flex-col items-center gap-4 py-6 bg-neutral-950 w-full pt-20'>
       <h3 className='text-white font-bold text-3xl'>
         TOP OF THE LINE <span className='text-red-500'>EQUIPIMENT</span>
       </h3>
@@ -67,14 +67,14 @@ Every piece of equipment at The Fitters is chosen for one reason: to help you li
      </p>
 
           {/*  nav */}
-     <div className='flex'>
+     <div className='flex p-1 skew-x-5 bg-neutral-800 gap-1 text-sm font-semibold mt-10'>
           <div onClick={(e)=>{
                e.preventDefault()
             setNav({state1:true,state2:false,state3:false})
             setProd(equipment);
           }}
-           className={`px-7 py-3 text-gray-300  hover:bg-white/50 skew-x-5 ${nav.state1 ? 'bg-neutral-800' : 'bg-red-700 '} `}>
-                <p className='-skew-x-3'>All Equipment</p>
+           className={`px-7 py-2 flex items-center text-gray-300 cursor-pointer    ${nav.state1 ? 'bg-red-700 hover:bg-red-700' : 'bg-neutral-800 hover:bg-white/50'} `}>
+                <p className='-skew-x-5'>All Equipment</p>
           </div>
           <div onClick={(e)=>{
             e.preventDefault()
@@ -87,14 +87,13 @@ Every piece of equipment at The Fitters is chosen for one reason: to help you li
             })
             )
            
-          }} className={` px-7 py-3 text-gray-300 hover:bg-white/50 skew-x-5 ${nav.state2 ? 'bg-neutral-800  ' : 'bg-red-700'}`}>
-                <p className='-skew-x-3'>PowerLifting</p>
+          }} className={` px-7 py-2 flex items-center text-gray-300 cursor-pointer ${nav.state2 ? 'bg-red-700' : 'bg-neutral-800 hover:bg-white/50'}`}>
+                <p className='-skew-x-5'>PowerLifting</p>
           </div>
           <div
            onClick={(e)=>{
-            setNav({state1:false,state2:false,state3:true})
-              e.preventDefault()
-            setNav({state1:false,state2:true,state3:false})
+            e.preventDefault()
+            setNav({state1:false,state2:false,state3:true}) 
             setProd( equipment.filter((eq)=>{
               if(eq.category == "accessories")
               return(
@@ -103,35 +102,35 @@ Every piece of equipment at The Fitters is chosen for one reason: to help you li
             })
             )
           }}
-           className={` px-7 py-3 text-gray-300 hover:bg-white/50 skew-x-5 ${nav.state3 ? 'bg-neutral-800' : 'bg-red-700'}`}>
-                <p className='-skew-x-3'>Accessories</p>
+           className={` px-7 py-2 flex items-center text-gray-300 cursor-pointer ${nav.state3 ? 'bg-red-700' : 'bg-neutral-800 hover:bg-white/50'}`}>
+                <p className='-skew-x-5'>Accessories</p>
           </div>
       </div>
 
+      <div className='relative grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 bg-neutral-950 w-full px-16 py-8'>
+        {products.map((card,index)=>{
+          return(
+            <div key={index} className='flex flex-col bg-gradient-to-br pb-4 skew-x-1  from-neutral-900 to-neutral-800 transition-all duration-300 cursor-default hover:scale-105 ease-in-out gap-3'>
+              <div className='relative w-full h-48 '>
+                <img src={card.image} className='object-cover w-full h-full' alt="" />
+                <span className='absolute top-5 bg-red-600 px-2 py-1 uppercase skew-x-12 text-white right-5 text-xs font-bold'><p className='-skew-x-12'>{card.category}</p></span>
+              </div>
+              <h1 className='text-white text-lg font-bold px-2  -skew-x-1'>{card.name}</h1>
+              <p className='text-neutral-500 text-sm px-2 -skew-x-1'>{card.description}</p>
+              <span className='text-red-400 text-sm px-2 -skew-x-1 uppercase'>Key Features:</span>
+              <div className='flex flex-col  text-neutral-400 text-sm font-medium px-2 -skew-x-1 '>
+                {card.specs.map((spec,index)=>{
+                  return(
+                    <p key={index} className='flex items-center '><span><Dot className='text-red-600'/></span>{spec}</p>
+                  )
+                })}
+              </div>
+            </div>
+          )
+        })}
 
-      <div className='flex gap-8 ml-20 flex-wrap w-full'>
-
-       {/*  the cards */}
-      {products.map((equip,key)=>(
-        
-      <div key={key} className='flex flex-col gap-4 w-[350px] h-[450px] bg-gradient-to-br from-neutral-900 to-neutral-800 mt-6 transition-all duration-100 hover:scale-105 ease-in-out'>
-
-        <div style={{  backgroundImage: `url(${equip.image})`}} className={` bg-cover bg-no-repeat bg-center h-[350px] w-[350px] relative`}>
-          <span className='p-1 bg-red-400 text-white skew-x-3  font-bold absolute top-0 right-0  '>{equip.category}</span>
-        </div>
-        <div className='flex flex-col gap-3 p-4'>
-        <span className='font-bold text-lg text-white'>{equip.name}</span>
-        <p className='text-gray-300  text-wrap'>{equip.description}</p>
-        <p  className='text-red-400'>key features</p>
-          <div className='flex flex-col  gap-3'>
-        {equip.specs.map((spec,key)=>{
-          return    <p key={key}  className='text-red-300'>{spec}</p>
-           
-        })} 
-        </div>
-        </div>
-      </div>))}
       </div>
+
     </div>
   )
 }
