f974b2d252c7047267616078d97eaed3dc240145
Kdus17 <kidus2460@gmail.com>
2025-08-21 19:43:01 +0300

    create customer

--- src/admin/CreateAnn.jsx
+++ src/admin/CreateAnn.jsx
@@ -1,34 +1,50 @@
 import { Save, X } from 'lucide-react'
 import React from 'react'
 
-export default function CreateAnn() {
+export default function CreateAnn({setSee}) {
   return (
-    <div className='flex flex-col border-y-1 gap-4 p-10 bg-neutral-800'>
-        <div className='flex justify-between text-white '>
-            <h1 className='text-2xl font-bold'>Create Announcement</h1>
-            <span><X/></span>
-        </div>
-        <div className='flex flex-col gap-1 text-white '>
-            <label htmlFor="title" className='font-semibold'>Title</label>
-            <input type="text" name="title" id="title" className='outline-none p-2 bg-neutral-700' />
-        </div>
-
-        <div className='flex flex-col gap-1 text-white '>
-            <label htmlFor="announcment" className='font-semibold'>Announcment</label>
-            <textarea cols={4} type="text" name="announcment" id="announcment" className='outline-none p-2 bg-neutral-700' />
-        </div>
-
-        <div className='flex justify-around '>
-            <div className='skew-x-2 hover:bg-red-600 text-red-600 hover:text-white p-2 cursor-pointer'>
-                <span className='-skew-x-2'>Cancel</span>
+        <div className="fixed inset-0 z-50 bg-black/80 backdrop-blur-sm flex items-center justify-center p-4">
+          <div variant="intense" padding="lg" className="md:w-full max-w-2xl">
+            <div className="flex justify-between items-center mb-6">
+              <h3 className="text-heading-3 font-bold text-white">Add New Customer</h3>
+              <X className='text-red-600 cursor-pointer' onClick={()=>{setSee(false)}}/>
             </div>
-            <div className='skew-x-2 bg-amber-600 text-white flex items-center p-2 cursor-pointer'>
-                <Save/>
-                <span className='-skew-x-2'>publish</span>
-            </div>
+            <form className="space-y-6">
+              <div className="grid md:grid-cols-2 gap-4 ">
+                <div >
+                  <label className="block text-lg font-semibold text-white mb-2">Full Name</label>
+                  <input label="Full Name" type="text" required placeholder="John Doe" className='border-red-600 text-white outline-none p-2 border rounded-md'/>
+                </div>
+                <div>
+                 <label className="block text-lg font-semibold text-white mb-2">Email</label>
+                 <input label="Email" type="email" required placeholder="john@example.com" className='border-red-600 text-white outline-none p-2 border rounded-md'/>  
+                </div>
+                
+              </div>
+              <div className="grid md:grid-cols-2 gap-4">
+                <div>
+                  <label className="block text-lg font-semibold text-white mb-2">Phone</label>
+                  <input label="Phone" type="tel" required placeholder="+251 911 123 456" className='outline-none border p-2 rounded-md border-red-600 text-white' />
+                </div>
+                
+                <div>
+                  <label className="block text-lg font-semibold text-neutral-200 mb-2">Membership Type</label>
+                  <select className=" p-2 w-full bg-neutral-900 border border-neutral-700 text-neutral-100 capitalize">
+                    <option value="active">active</option>
+                    <option value="inactive">inactive</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex justify-end space-x-12 mt-16">
+                <button className='p-2 rounded-md bg-red-500 text-white hover:-translate-y-1 transition-all duration-300 ease-in-out cursor-pointer' onClick={()=>{setSee(false)}}>
+                  Cancel
+                </button>
+                <button className='p-2 rounded-md bg-blue-500 text-white hover:-translate-y-1 transition-all duration-300 ease-in-out cursor-pointer'>
+                  Add Customer
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-
-
-    </div>
   )
 }
